0f9f659430c8624f3e17f385307223cd300b7311
Pascal Kaufmann <p.kaufmann.dev@gmail.com>
2016-12-04 15:40:17 +0800

    add basic auth to express static file serving

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var path = require("path");
+var auth = require("http-auth");
+
+var basic = auth.basic({
+    realm: "my realm"
+  }, (username, password, callback) => { 
+    // Custom authentication 
+    // Use callback(error) if you want to throw async error. 
+    callback(username === "merkel" && password === "de");
+  }
+);
+
 
 var app = express();
-app.use(express.static(path.join(__dirname,"../app/dist")));
-app.listen(7777,function(){
+app.use(auth.connect(basic));
+app.use(express.static(path.join(__dirname,"../dist")));
+app.listen(process.env.PORT || 7777,function(){
     console.log("Started listening on port", 7777);
 })
