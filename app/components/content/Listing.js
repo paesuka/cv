e3b11c87013d5463008e7e74134c2cab45676d9b
Pascal Kaufmann <p.kaufmann.dev@gmail.com>
2016-12-04 16:56:22 +0800

    add languages and support list for listings

--- app/components/content/Listing.js
+++ app/components/content/Listing.js
@@ -2,15 +2,45 @@ import React from 'react';
 
 export default class Listing extends React.Component {
   // TODO use destructuring
-    render() {
-        const sections = this.props.data.sections.map((data, i) => {
+    createListing(content, idx) {
+      if (Array.isArray(content)) {
+        return (
+          <ul key={idx + 2}>
+          {content.map((contentData, i) => {
+            if (Array.isArray(contentData)) {
+              return this.createListing(contentData);
+            } else {
+              return (
+                <li key={i} dangerouslySetInnerHTML = {{__html: contentData}} />
+              );
+            }
+          })
+        }
+        </ul>
+      );
+      } else {
+        return (
+          <p dangerouslySetInnerHTML = {{__html: content}} />
+        );
+      }
+    }
+    createSection(sections) {
+        return sections.map((data, i) => {
+            const listing = this.createListing(data.content, i);
             return (
                 <section key={i}>
                     <h2>{data.title}</h2>
-                    <p dangerouslySetInnerHTML = {{__html: data.content}} />
+                    <div>
+                        <h3 dangerouslySetInnerHTML = {{__html: data.subtitle}} />
+                        {listing}
+                    </div>
                 </section>
             );
-        });
+        })
+    }
+
+    render() {
+        const sections = this.createSection(this.props.data.sections);
         return (
             <article>
                 <header>
